7b0051ac92028999409f94a55731aa1b96473c6b
Roger Butcavage <roger.butcavage@gmail.com>
2015-10-05 10:44:10 -0400

    Add preliminary javascript file

---
+++ home.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  printIntroMessages();
+}
+
+function printIntroMessage() {
+  console.log("Hello Michael P.");
+  console.log("This is much better, don't you think?");
+}
