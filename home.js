663a97e364d80de64ddfa41d9273c8f14147f979
Roger Butcavage <roger.butcavage@gmail.com>
2015-10-05 16:29:42 -0400

    Add quarters function

--- home.js
+++ home.js
@@ -6,3 +6,8 @@ function printPageLoadMessages() {
   console.log("Hello Michael P.");
   console.log("This is much better, don't you think?");
 }
+
+function whatShouldIDoWithTheseQuarters() {
+  console.log("Go play your favorite old-school video game.");
+  console.log("Actually, before playing, check behind the machine.");
+}
