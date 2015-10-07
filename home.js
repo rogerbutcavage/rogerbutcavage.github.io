36c91480221ca019a4ad4af625818f7ca87c1aa2
Roger Butcavage <roger.butcavage@gmail.com>
2015-10-07 11:22:07 -0400

    Brush up text

--- home.js
+++ home.js
@@ -3,11 +3,19 @@ $(document).ready(function() {
 })
 
 function printPageLoadMessages() {
-  console.log("Hello Michael P.");
+  console.log("Hello again, Michael P.");
   console.log("This is much better, don't you think?");
+  console.log("\n\n");
+  console.log("You are about to embark on a serious mission.");
+  console.log("If you ever get stuck, feel free to email me, Roger.");
+  console.log("I'm here to help.");
+  console.log("\n\n");
+  console.log("Now, go find something I left for you in the bathroom.");
 }
 
 function whatShouldIDoWithTheseQuarters() {
   console.log("Go play your favorite old-school video game.");
-  console.log("Actually, before playing, check behind the machine.");
+  console.log("It's a short walk away.");
+  console.log("Leaving at your usual time would be great.");
+  console.log("And actually, before playing, check behind the machine.");
 }
