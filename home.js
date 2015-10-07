65c838108b1594276c528abe2e3380adfebfdb48
Roger Butcavage <roger.butcavage@gmail.com>
2015-10-07 15:43:03 -0400

    Minor copy change

--- home.js
+++ home.js
@@ -10,6 +10,10 @@ function printPageLoadMessages() {
   console.log("If you ever get stuck, feel free to email me, Roger.");
   console.log("I'm here to help.");
   console.log("\n");
+  console.log("Also, even though the secrecy of this mission is paramount,");
+  console.log("feel free to photograph your journey along the way,")
+  console.log("if you so desire.");
+  console.log("\n");
   console.log("Now, go find something I left for you in the bathroom.");
 }
 
